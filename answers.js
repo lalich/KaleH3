c7ff18d62b4ad6abb302b8396324212dac310b22
Mark D. Lalich <mdlalich@gmail.com>
2023-05-01 23:05:49 -0700

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -27,8 +27,23 @@ for (let i = 0; i <=200; i++){
 }
 
 ////////////////////////////////
-// Fizz Buzz
+// Fizz Buzz - Too Date best exercise outcome yet!!!
 ////////////////////////////////
+for (let i = 1; i <=100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log('FizzBuzz ' + i);}
+        
+        else; if (i % 3 == 0 && i % 5 !== 0)
+        console.log('Fizz ' + i);
+        
+        else; if (i % 5 == 0 && i % 3 !== 0)
+        console.log('Buzz ' + i)
+        
+        else; if (i % 3 !== 0 && i % 5 !== 0)
+        console.log(i)
+
+    }
+ 
 
 
 ////////////////////////////////
